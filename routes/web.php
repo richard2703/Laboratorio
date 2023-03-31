2afee0bfeedaab4d4f2d4185de1924b1e31b499a
Ricardo Rios <rrios@suappi.com.mx>
2023-03-31 16:21:49 -0600

    buscador de examenes al crear ticket

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('pacientes', App\Http\Controllers\pacientesController::class);
     Route::resource('tickets', App\Http\Controllers\ticketsController::class);
     Route::get('create/tickets/search', [App\Http\Controllers\ticketsController::class, 'search'])->name('search.ticket.paciente');
+    Route::get('create/tickets/searchexamen', [App\Http\Controllers\ticketsController::class, 'searchexamen'])->name('search.ticket.examen');
     Route::get('/ticket/{ticket}/examenes', [App\Http\Controllers\resultadosController::class, 'index'])->name('resultados.index');
     Route::post('/ticket/{ticket}/resultados', [App\Http\Controllers\resultadosController::class, 'create'])->name('resultados.create');
     Route::post('/ticket/resultados', [App\Http\Controllers\resultadosController::class, 'store'])->name('resultados.store');
